d9d0b7421b8f19ba7fbcc505ed483928690de63c
kevin del cid <danieldelcidf@gmail.com>
2023-03-20 23:15:12 -0600

    Add english and spanish command 'change to spanish... english' 'cambia a español, ingles'

--- App.tsx
+++ App.tsx
@@ -1,88 +1,113 @@
 import axios from 'axios';
 
 import { useState, useEffect } from 'react';
-import { Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, LogBox, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 // import * as Speech from 'expo-speech';
-import Tts  from 'react-native-tts';
+import Tts from 'react-native-tts';
 import { PermissionsAndroid } from 'react-native';
 import Voice from '@react-native-voice/voice';
 
 
 const BASE_API_URL = 'https://dobbyassistantbackend-production.up.railway.app/'
-
+LogBox.ignoreLogs(['new NativeEventEmitter()']);
 
 
 export default function App() {
 
-  const [ question, setQuestion ] = useState('')
-  const [ gptResponse, setGptResponse ] = useState('')
- 
+  const [question, setQuestion] = useState('')
+  const [gptResponse, setGptResponse] = useState('')
+
   const [result, setResult] = useState('');
   const [error, setError] = useState('');
   const [isRecording, setIsRecording] = useState(false);
 
 
 
-  useEffect(()=>{
+  useEffect(() => {
     Tts.setDefaultLanguage('es-MX');
-    Tts.speak('Hello, world!');
-  },[])
+    
+  }, [])
 
   useEffect(() => {
-    
-    fetchGptResponse(result)
+
+
+    if (result.toLowerCase() === "cambia a inglés") {
+      Tts.setDefaultLanguage('en-US');
+      speak("Sure! i speak english with you")
+    }
+    else if (result.toLowerCase() === "change to english") {
+      Tts.setDefaultLanguage('en-US');
+      speak("Sure! i speak english with you")
+    }
+
+    else if (result.toLowerCase() === "cambia a español") {
+      Tts.setDefaultLanguage('es-MX');
+      speak("Claro, puedo hablar español")
+    }
+    else if (result.toLowerCase() === "change to spanish") {
+      Tts.setDefaultLanguage('es-MX');
+      speak("Claro, puedo hablar español")
+    }
+    else {
+      if(result)
+      fetchGptResponse(result)
+    }
+
+
+
   }, [result])
-  
+
+
   Voice.onSpeechStart = () => setIsRecording(true)
   Voice.onSpeechEnd = () => setIsRecording(false)
   Voice.onSpeechError = (error: any) => setError(error.error)
   Voice.onSpeechResults = (result: any) => setResult(result.value[0])
 
 
-  const startRecording = async ()=>{
-    if(Voice !== null){
-    try {
-      await Voice.start('es-MX')
-    } catch (error : any) {
-      setError(error.message)
+  const startRecording = async () => {
+    if (Voice !== null) {
+      try {
+        await Voice.start('es-MX')
+      } catch (error: any) {
+        setError(error.message)
+      }
     }
   }
-  }
-  const stopRecording = async ()=>{
-    if(Voice !== null){
-    try {
-      await Voice.stop()
+  const stopRecording = async () => {
+    if (Voice !== null) {
+      try {
+        await Voice.stop()
 
-    } catch (error) {
-      console.log(error)
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
-  }
 
-  const speak = (gptWords:string) => {
+  const speak = (gptWords: string) => {
 
     // const speechOptions = {
     //   language: 'es-MX'
     // };
     Tts.speak(gptWords);
-   
+
   };
 
-  const fetchGptResponse =  (question:string)=>{
+  const fetchGptResponse = (question: string) => {
     axios.get(`${BASE_API_URL}userSays=${question}`).then(response => {
       console.log(response.data)
       setGptResponse(response.data)
       speak(response.data)
-     })
+    })
 
   }
 
 
 
-  const handleSubmit =() =>{
+  const handleSubmit = () => {
     console.log(`${BASE_API_URL}userSays=${question}`)
 
-   
+
     fetchGptResponse(question)
 
   }
@@ -103,10 +128,10 @@ export default function App() {
         console.log('Audio Permission denied');
       }
     } catch (error) {
-      console.warn(error);
+      console.log(error);
     }
   };
-  
+
 
 
   useEffect(() => {
@@ -116,30 +141,30 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>{ gptResponse || "Type your question for Dobby" }</Text>
-       <TextInput
+      <Text>{gptResponse || "Type your question for Dobby"}</Text>
+      <TextInput
         style={styles.input}
         onChangeText={setQuestion}
         value={question}
         multiline={true}
         placeholder="useless placeholder"
-        
+
       />
       <Button title='enviar pregunta' onPress={handleSubmit} />
       {/* <Button title="Press to hear some words" onPress={speak()} /> */}
-      <Button title='stop' onPress={()=>Tts.stop()} />
+      <Button title='stop' onPress={() => Tts.stop()} />
 
-    <Text>{result}</Text>
-    <Text>{error}</Text>
+      <Text>{result}</Text>
+      <Text>{error}</Text>
 
 
 
-    <Pressable style={styles.button} onPress={isRecording ? stopRecording : startRecording} >
-      <Text>{isRecording ? "Stop recording" : "Start Recording"}</Text>
-    </Pressable>
+      <Pressable disabled={isRecording} style={{...styles.button, backgroundColor: isRecording ? "gray" : "red"  }} onPress={startRecording} >
+        <Text>{isRecording ? "listening" : "Start Recording"}</Text>
+      </Pressable>
 
-   
-    
+
+
     </View>
   );
 }
@@ -151,9 +176,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  button:{
+  button: {
     backgroundColor: "red",
-    padding:10
+    padding: 10
   },
   input: {
     height: "auto",
